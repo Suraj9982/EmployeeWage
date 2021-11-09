a5333fc830cc88bc42c4a6d34eaeee7a0c541e7f
suraj9982 <surajsk9982@gmail.com>
2021-11-09 10:33:51 +0530

    [suraj] add : new function added for check attendance

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -8,5 +8,19 @@ namespace EmployeeWageComputation
 {
     class EmployeeWageCalculation
     {
+        const int IS_PRESENT = 1;
+        Random random = new Random();
+        public void checkattendance()
+        {
+            int check = random.Next(0, 2);
+            if (check == IS_PRESENT)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
     }
 }
