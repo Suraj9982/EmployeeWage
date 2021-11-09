440da6da446e8e810b4826f55550691c95d2c818
suraj9982 <surajsk9982@gmail.com>
2021-11-09 11:08:17 +0530

    [suraj] add : parttime employee added

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -8,20 +8,27 @@ namespace EmployeeWageComputation
 {
     class EmployeeWageCalculation
     {
-        const int IS_PRESENT = 1,WAGE_PER_HR=20,FULL_HR=8;
+        const int IS_FULLTIME = 0,IS_PARTTIME=1,WAGE_PER_HR=20,FULLTIME_HR=8,PARTTIME_HR=4;
         Random random = new Random();
         public void dailyemployeewage()
         {
-            int check = random.Next(0, 2);
-            if (check == IS_PRESENT)
+            int check = random.Next(0, 3);
+            int emp_hours=0;
+            if (check == IS_FULLTIME)
             {
-                int Dailywage = WAGE_PER_HR * FULL_HR;
-                Console.WriteLine("emmloyee is dailywage is" + Dailywage);
+                emp_hours += FULLTIME_HR;
+            }
+            if(check==IS_PARTTIME)
+            {
+                emp_hours += PARTTIME_HR;
             }
             else
             {
                 Console.WriteLine("employee is absent");
+                emp_hours = 0;
             }
+            int Dailywage = WAGE_PER_HR * emp_hours;
+            Console.WriteLine("employee is daily wage is " + Dailywage);
         }
     }
 }
