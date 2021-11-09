c4943b571ec43b4585812d9eed499c82f859659e
suraj9982 <surajsk9982@gmail.com>
2021-11-09 11:31:31 +0530

    [suraj] add : new function for checking monthly wage

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -8,26 +8,32 @@ namespace EmployeeWageComputation
 {
     class EmployeeWageCalculation
     {
-        const int IS_FULLTIME = 0,IS_PARTTIME=1,WAGE_PER_HR=20,FULLTIME_HR=8,PARTTIME_HR=4;
+        const int IS_FULLTIME = 0,IS_PARTTIME=1,WAGE_PER_HR=20,FULLTIME_HR=8,PARTTIME_HR=4,TOTAL_DAYS=20;
         Random random = new Random();
-        public void dailyemployeewageswitch()
+        public void monthlyemployeewages()
         {
-            int check = random.Next(0, 3);
-            int emp_hours=0, Dailywage;
-            switch (check)
+            int emp_hours = 0, Dailywage=0,Monthlywage = 0;
+            for (int day = 0; day < TOTAL_DAYS; day++)
             {
-                case IS_FULLTIME:
-                    emp_hours += FULLTIME_HR;
-                    break;
-                case IS_PARTTIME:
-                    emp_hours += PARTTIME_HR;
-                    break;
-                default:
-                    emp_hours = 0;
-                    break;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case IS_FULLTIME:
+                        emp_hours += FULLTIME_HR;
+                        Console.WriteLine("emolyee is fulltime");
+                        break;
+                    case IS_PARTTIME:
+                        emp_hours += PARTTIME_HR;
+                        Console.WriteLine("employee is parttime");
+                        break;
+                    default:
+                        emp_hours = 0;
+                        break;
+                }
+                Dailywage = WAGE_PER_HR * emp_hours;
+                Monthlywage += Dailywage;
             }
-            Dailywage = WAGE_PER_HR * emp_hours;
-            Console.WriteLine("employee is daily wage is " + Dailywage);
+            Console.WriteLine("employee is monthly wage is " + Monthlywage);
         }
     }
 }
