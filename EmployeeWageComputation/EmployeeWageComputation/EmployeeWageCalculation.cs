ded9e72cd0a4a4d2fc89581784dab2cbc31d2aff
suraj9982 <surajsk9982@gmail.com>
2021-11-16 23:10:18 +0530

    [suraj] add : This saves the employee age of multiple companies

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -6,14 +6,27 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
+   
     class EmployeeWageCalculation
     {
-        const int IS_FULLTIME = 0,FULLTIME_HR=8,PARTTIME_HR=4, IS_PARTTIME=1;
+        public const int IS_FULLTIME = 0, FULLTIME_HR = 8, PARTTIME_HR = 4, IS_PARTTIME = 1;
+        private string Company;
+        private int WAGE_PER_HR;
+        private int MAX_WORKING_DAYS;
+        private int MAX_WORKING_HOURS;
+        private int monthlywage; 
         Random random = new Random();
-        public void employeewagestillcondition(string Company, int WAGE_PER_HR,int MAX_WORKING_DAYS,int MAX_WORKING_HOURS)
+        public EmployeeWageCalculation(string Company,int WAGE_PER_HR,int MAX_WORKING_DAYS, int MAX_WORKING_HOURS)
         {
-            int emp_hours = 0, dailywage=0,monthlywage = 0,emp_days=0;
-            while(emp_days < MAX_WORKING_DAYS && emp_hours < MAX_WORKING_HOURS)
+            this.Company = Company;
+            this.WAGE_PER_HR = WAGE_PER_HR;
+            this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+            this.MAX_WORKING_HOURS = MAX_WORKING_HOURS;
+        }       
+        public void ComputeEmployeeWage()
+        {
+            int emp_hours = 0, dailywage=0,emp_days=0;
+            while(emp_days < this.MAX_WORKING_DAYS && emp_hours < this.MAX_WORKING_HOURS)
             {
                 int check = random.Next(0, 3);
                 switch (check)
@@ -28,11 +41,15 @@ namespace EmployeeWageComputation
                         emp_hours += 0;
                         break;
                 }
-                dailywage = WAGE_PER_HR * emp_hours;
+                dailywage = this.WAGE_PER_HR * emp_hours;
                 monthlywage += dailywage;
                 emp_days++;
             }
-            Console.WriteLine("The monthly wage of {0} is {1} ",Company,monthlywage);
+            Console.WriteLine("The monthly wage of:" + this.Company + "is" + this.monthlywage);
+        }
+        public string tostring()
+        {
+            return "The monthly wage of:"+this.Company + "is" + this.monthlywage;
         }
     }
 }
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -7,21 +7,12 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to employee wage computation");
-            Console.WriteLine("how many companies you want to enter");
-            int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < num; i++)
-            {
-                EmployeeWageCalculation emp = new EmployeeWageCalculation();
-                Console.WriteLine("enter the company name");
-                string company = Console.ReadLine();
-                Console.WriteLine("enter the rate per hour");
-                int rate_per_hour = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("enter the maximum working days");
-                int max_days = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("enter the maximum working hours");
-                int max_hr = Convert.ToInt32(Console.ReadLine());
-                emp.employeewagestillcondition(company, rate_per_hour, max_days, max_hr);
-            }
+            EmployeeWageCalculation relaince = new EmployeeWageCalculation("relaince",15,25,100);
+            EmployeeWageCalculation tata = new EmployeeWageCalculation("tata", 15, 25, 100);
+            relaince.ComputeEmployeeWage();
+            Console.WriteLine(relaince.tostring());
+            tata.ComputeEmployeeWage();
+            Console.WriteLine(tata.tostring());
         }
     }
 }
